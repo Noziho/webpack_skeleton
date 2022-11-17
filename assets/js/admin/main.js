3654a8f1d221e290226640357e4b2c639740b7a3
Noziho <noah.decroix3@gmail.com>
2022-11-17 11:41:42 +0100

    generic skeleton

--- assets/js/admin/main.js
+++
@@ -1,9 +0,0 @@
-import {data1, data2} from "../common/data";
-import '../../css/admin-style.css';
-/*
-    Seconde manière de faire :
-    import data2, {data1 as data3} from "../common/data";
- */
-
-data1.describe();
-console.log(data2);
